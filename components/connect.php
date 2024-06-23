a91be32a6f6d8a3fcf9089ea00cbb3c4324a2eb7
tamourax <92335160+tamourax@users.noreply.github.com>
2024-06-23 17:23:21 +0300

    web2

---
+++ components/connect.php
@@ -0,0 +1,129 @@
+<?php
+
+
+
+$dsn = 'mysql:host=localhost;dbname=myweb';
+$username = 'root';
+$password = '';
+
+try {
+    // Create a new PDO instance
+    $conn = new PDO($dsn, $username, $password);
+
+    // Set error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+// Create connection
+$conn = new PDO($dsn, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL statements
+$sql = "
+CREATE TABLE `bookmark` (
+  `user_id` varchar(20) NOT NULL,
+  `playlist_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `comments` (
+  `id` varchar(20) NOT NULL,
+  `content_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `tutor_id` varchar(20) NOT NULL,
+  `comment` varchar(1000) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `contact` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` int(10) NOT NULL,
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `content` (
+  `id` varchar(20) NOT NULL,
+  `tutor_id` varchar(20) NOT NULL,
+  `playlist_id` varchar(20) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `video` varchar(100) NOT NULL,
+  `thumb` varchar(100) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(20) NOT NULL DEFAULT 'deactive'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `likes` (
+  `user_id` varchar(20) NOT NULL,
+  `tutor_id` varchar(20) NOT NULL,
+  `content_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `playlist` (
+  `id` varchar(20) NOT NULL,
+  `tutor_id` varchar(20) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `thumb` varchar(100) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(20) NOT NULL DEFAULT 'deactive'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tutors` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `profession` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+COMMIT;
+";
+
+// Execute multi-query
+// if ($conn->multi_query($sql) === TRUE) {
+//     echo "Tables created successfully";
+// } else {
+//     echo "Error creating tables: " . $conn->error;
+// }
+
+// // Close connection
+// $conn->close();
+// $sql = "INSERT INTO users (id, name, email,password)
+// VALUES ('1', 'mina', 'mina@example.com','123456789')";
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+
+   function unique_id() {
+      $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+      $rand = array();
+      $length = strlen($str) - 1;
+      for ($i = 0; $i < 20; $i++) {
+          $n = mt_rand(0, $length);
+          $rand[] = $str[$n];
+      }
+      return implode($rand);
+   }
+
+?>
